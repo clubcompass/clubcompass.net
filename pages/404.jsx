3a13e444a063171cbd592f2e8ab87b28fa9b5333
AndrewH06 <andrew.z.hale1@gmail.com>
2022-01-04 19:12:19 -0800

    🪄 Adjusted 404 page
    Addressing changes:
    - Made "Go back home" link
    - Made everything smaller
    
    closes: #106

--- pages/404.jsx
+++ pages/404.jsx
@@ -4,23 +4,26 @@ import Image from "next/image";
 const Custom404 = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="mb-6 p-4 relative bg-[#E7EEFF] rounded-xl">
-        <div className="mt-[6px] mx-[3px]">
-          <Image src="/cc-404.svg" alt="logo" width={100} height={100} />
+      <div className="mb-6 p-3 relative bg-[#E7EEFF] rounded-xl">
+        <div className="mt-[4px] mx-[5px]">
+          <Image src="/cc-404.svg" alt="logo" width={64} height={64} />
         </div>
       </div>
       <p className="font-bold text-[#1C5EF9] tracking-wider text-lg">
         404 ERROR
       </p>
-      <h1 className="font-extrabold text-black text-6xl mb-2">
+      <h1 className="font-extrabold text-black text-5xl mb-2">
         Page not found.
       </h1>
-      <p className="text-[#6D7481] tracking-wider mb-6 text-lg">
+      <p className="text-[#6D7481] tracking-wider mb-6 text-md">
         Sorry, we couldn't find the page you're looking for.
       </p>
-      <p className="font-semibold text-[#1C5EF9] tracking-wide text-lg">
+      <a
+        href="/ "
+        className="font-semibold text-[#1C5EF9] tracking-wide text-lg"
+      >
         Go back home &rarr;
-      </p>
+      </a>
     </div>
   );
 };
