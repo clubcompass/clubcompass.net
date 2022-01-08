42da5850431e9bb29ea90f4f4737db00fa75200e
AndrewH06 <andrew.z.hale1@gmail.com>
2022-01-08 12:56:02 -0800

    🧩 NavLeft (#161)
    Addressing changes:
    - Created `NavLeft` component
    - Logo and Title
    - Links to homepage
    
    Additional changes:
    - Updated `react-spring`
    
    closes: #161

---
+++ components/layout/navigation/NavLeft.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const NavLeft = () => {
+  return (
+    <Link href="/">
+      <a className="flex items-center">
+        <a className="w-[40px] h-[40px] mr-5">
+          <Image src="/cc-auth.svg" alt="cc" width={40} height={40} />
+        </a>
+        <h1 className="text-2xl font-bold">Club Compass</h1>
+      </a>
+    </Link>
+  );
+};
