1477c5bbf013a7ab8a40475918ac82b162553405
AndrewH06 <andrew.z.hale1@gmail.com>
2022-02-25 09:15:37 -0800

    🪄 Descripition at top on Mobile View
    Addressing changes:
    - Placed description at top of club page for mobile view

--- components/pages/club/ClubWrapper.jsx
+++ components/pages/club/ClubWrapper.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { useBreakpoints } from "../../../hooks";
 
 export const ClubWrapper = ({ children, availability }) => {
   const [header, contact, meeting, content, members, similar] = children;
+  const { isMd, isSm, isXs } = useBreakpoints();
+  const isMobile = isMd || isSm || isXs;
+
   return (
     <div className="mt-4 flex flex-col gap-8">
       <div className="flex flex-col w-full md:flex-row md:justify-between items-center">
@@ -10,8 +14,9 @@ export const ClubWrapper = ({ children, availability }) => {
         <Button availability={availability} />
       </div>
       <div className="grid grid-rows lg:grid-cols-6 lg:gap-8">
+        {isMobile && <Card title="Description">{content}</Card>}
         <div className="order-last lg:order-first lg:col-span-4">
-          <Card title="Description">{content}</Card>
+          {!isMobile && <Card title="Description">{content}</Card>}
           {members}
         </div>
         <div className="md:grid md:grid-cols-2 md:gap-8 lg:grid-cols-1 lg:gap-0 lg:col-span-2">
