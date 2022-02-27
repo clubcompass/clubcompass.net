aaaf595b9139cecc3e50a988b4866ff5a9974ba5
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2022-02-26 17:19:52 -0800

    🛠️ Update create club route (#253)
    Addressing changes:
    - updated route to support missing information
    
    closes : #253

--- pages/api/club/create.js
+++ pages/api/club/create.js
@@ -7,10 +7,8 @@ export default async (req, res) => {
     description,
     meetingDate,
     location,
-    president,
-    vicePresident,
-    secretary,
-    treasurer,
+    availability,
+    presidentId,
     memberIds,
     tagIds,
     teacherId,
@@ -27,39 +25,130 @@ export default async (req, res) => {
     links,
   } = req.body;
 
-  const tags = Array.from([...tagIds], (tagId) => {
-    return {
-      id: tagId,
+  let initialClubQuery = {};
+
+  if (name !== undefined) {
+    initialClubQuery.name = name;
+    initialClubQuery.slug = name
+      .toString()
+      .toLowerCase()
+      .replace(/\s+/g, "-")
+      .replace(/[^\w\-]+/g, "")
+      .replace(/\-\-+/g, "-")
+      .replace(/^-+/, "")
+      .replace(/-+$/, "");
+  }
+
+  if (email !== undefined) {
+    initialClubQuery.email = email;
+  }
+
+  if (description !== undefined) {
+    initialClubQuery.email = email;
+  }
+
+  if (meetingDate !== undefined) {
+    initialClubQuery.email = email;
+  }
+
+  if (location !== undefined) {
+    initialClubQuery.location = location;
+  }
+
+  if (tagIds !== undefined) {
+    const tags = Array.from([...tagIds], (tagId) => {
+      return {
+        id: tagId,
+      };
+    });
+
+    initialClubQuery.tags = {
+      connect: tags,
     };
-  });
+  }
 
-  const members = Array.from([...memberIds], (memberId) => {
-    return {
-      id: memberId,
+  if (links !== undefined) {
+    initialClubQuery.links = {
+      create: links,
     };
-  });
+  }
 
-  const editors = [
-    { id: president.id },
-    { id: vicePresident.id },
-    { id: secretary.id },
-  ];
+  if (availability !== undefined) {
+    initialClubQuery.availability = availability;
+  }
 
-  let applicationInfo = {
-    teacher: {
-      connect: {
-        id: teacherId,
+  initialClubQuery.roles = {
+    create: [
+      {
+        name: "president",
+        color: "#C3F4E9",
+        type: "LEADERSHIP",
+        description:
+          "Provides leadership and direction to the club organization. Understand the club operating guidelines.",
       },
-    },
-    purpose: purpose,
-    membershipRequirements: membershipRequirements,
-    dutiesOfMembers: dutiesOfMembers,
-    titlesAndDutiesOfOfficers: titlesAndDutiesOfOfficers,
-    selectionOfOfficers: selectionOfOfficers,
-    officerMinimumGPA: officerMinimumGPA,
-    percentAttendanceForOfficialMeeting: percentAttendanceForOfficialMeeting,
-    percentAttendanceToApproveDecision: percentAttendanceToApproveDecision,
+      {
+        name: "vice president",
+        color: "#FFEAB4",
+        type: "LEADERSHIP",
+        description: "Assists president in leading and managing the club.",
+      },
+      {
+        name: "secretary",
+        color: "#F3DCFE",
+        type: "LEADERSHIP",
+        description:
+          "Schedules and coordinates club activities and meetings. Provides assistance to president.",
+      },
+      {
+        name: "treasurer",
+        color: "#FFDCE5",
+        type: "LEADERSHIP",
+        description: "Manages the club's revenue and expenses.",
+      },
+    ],
   };
+
+  let applicationInfo = {};
+
+  if (teacherId !== undefined) {
+    applicationInfo.teacherId = {
+      connect: teacherId,
+    };
+  }
+
+  if (purpose !== undefined) {
+    applicationInfo.purpose = purpose;
+  }
+
+  if (membershipRequirements !== undefined) {
+    applicationInfo.membershipRequirements = membershipRequirements;
+  }
+
+  if (dutiesOfMembers !== undefined) {
+    applicationInfo.dutiesOfMembers = dutiesOfMembers;
+  }
+
+  if (titlesAndDutiesOfOfficers !== undefined) {
+    applicationInfo.titlesAndDutiesOfOfficers = titlesAndDutiesOfOfficers;
+  }
+
+  if (selectionOfOfficers !== undefined) {
+    applicationInfo.selectionOfOfficers = selectionOfOfficers;
+  }
+
+  if (officerMinimumGPA !== undefined) {
+    applicationInfo.officerMinimumGPA = officerMinimumGPA;
+  }
+
+  if (percentAttendanceForOfficialMeeting !== undefined) {
+    applicationInfo.percentAttendanceForOfficialMeeting =
+      percentAttendanceForOfficialMeeting;
+  }
+
+  if (percentAttendanceToApproveDecision !== undefined) {
+    applicationInfo.percentAttendanceToApproveDecision =
+      percentAttendanceToApproveDecision;
+  }
 
   if (projectedRevenue !== undefined) {
     applicationInfo.projectedRevenue = {
@@ -73,142 +162,58 @@ export default async (req, res) => {
     };
   }
 
-  let club = {
-    data: {
-      name: name,
-      slug: name
-        .toString()
-        .toLowerCase()
-        .replace(/\s+/g, "-")
-        .replace(/[^\w\-]+/g, "")
-        .replace(/\-\-+/g, "-")
-        .replace(/^-+/, "")
-        .replace(/-+$/, ""),
-      email: email,
-      description: description,
-      meetingDate: meetingDate,
-      location: location,
-      applicationInfo: {
-        create: applicationInfo,
-      },
-      roles: {
-        create: [
-          {
-            name: "president",
-            color: president.color,
-            type: "LEADERSHIP",
-            description: president.description,
-          },
-          {
-            name: "vice president",
-            color: vicePresident.color,
-            type: "LEADERSHIP",
-            description: vicePresident.description,
-          },
-          {
-            name: "secretary",
-            color: secretary.color,
-            type: "LEADERSHIP",
-            description: secretary.description,
-          },
-          {
-            name: "treasurer",
-            color: treasurer.color,
-            type: "LEADERSHIP",
-            description: treasurer.description,
-          },
-        ],
-      },
-      tags: {
-        connect: tags,
-      },
-      members: {
-        connect: members,
-      },
-      editors: {
-        connect: editors,
-      },
-    },
+  initialClubQuery.applicationInfo = applicationInfo;
+
+  const initialClubResponse = await prisma.club.create({
+    data: initialClubQuery,
     include: {
       roles: true,
     },
-  };
+  });
 
-  if (links !== undefined) {
-    club.data.links = {
-      create: links,
-    };
+  console.log(initialClubResponse);
+
+  if (memberIds !== undefined) {
+    for (let i = 0; i < memberIds.length; i++) {
+      await prisma.user.update({
+        where: {
+          ccid: memberIds[i],
+        },
+        data: {
+          invites: {
+            create: {
+              club: {
+                connect: {
+                  id: initialClubResponse.id,
+                },
+              },
+            },
+          },
+        },
+        include: {
+          invites: {
+            include: {
+              club: true,
+            },
+          },
+        },
+      });
+    }
   }
 
-  const initialClub = await prisma.club.create(club);
-
   await prisma.user.update({
     where: {
-      id: president.id,
+      id: presidentId,
     },
     data: {
       roles: {
         connect: {
-          id: initialClub.roles[0].id,
+          id: initialClubResponse.roles[0].id,
         },
       },
       clubs: {
         connect: {
-          id: initialClub.id,
-        },
-      },
-    },
-  });
-
-  await prisma.user.update({
-    where: {
-      id: vicePresident.id,
-    },
-    data: {
-      roles: {
-        connect: {
-          id: initialClub.roles[1].id,
-        },
-      },
-      clubs: {
-        connect: {
-          id: initialClub.id,
-        },
-      },
-    },
-  });
-
-  await prisma.user.update({
-    where: {
-      id: secretary.id,
-    },
-    data: {
-      roles: {
-        connect: {
-          id: initialClub.roles[2].id,
-        },
-      },
-      clubs: {
-        connect: {
-          id: initialClub.id,
-        },
-      },
-    },
-  });
-
-  await prisma.user.update({
-    where: {
-      id: treasurer.id,
-    },
-    data: {
-      roles: {
-        connect: {
-          id: initialClub.roles[3].id,
-        },
-      },
-      clubs: {
-        connect: {
-          id: initialClub.id,
+          id: initialClubResponse.id,
         },
       },
     },
@@ -216,7 +221,7 @@ export default async (req, res) => {
 
   const response = await prisma.club.findUnique({
     where: {
-      id: initialClub.id,
+      id: initialClubResponse.id,
     },
     include: {
       applicationInfo: {
