c82a980e10c311e3f90061697b531333d543cbee
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2022-02-28 19:45:23 -0800

    🐛 update all clubs cache upon user signup

--- pages/api/club/removeSignup.js
+++ pages/api/club/removeSignup.js
@@ -1,4 +1,5 @@
 import { prisma } from "../../../config/prisma";
+import { updateClubCache } from "../../../utils/cache/updateClubCache";
 
 export default async (req, res) => {
   const { userId, clubId } = req.body;
@@ -46,18 +47,7 @@ export default async (req, res) => {
     },
   });
 
-  // const response = await prisma.user.update({
-  //   where: {
-  //     id: userId,
-  //   },
-  //   data: {
-  //     clubs: {
-  //       disconnect: {
-  //         id: clubId,
-  //       },
-  //     },
-  //   },
-  // });
+  await updateClubCache();
 
   return res.status(200).json({ ...response });
 };
