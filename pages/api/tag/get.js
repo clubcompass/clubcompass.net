0abd7a3d11e604427a6739ef6f75637e40aeb8f8
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2022-03-05 11:10:28 -0800

    🪄 Option to get count of approved clubs when fetching tags (#261)
    Addressing changes:
    - get tags function
    - db driver get tags
    
    closes: #261

--- pages/api/tag/get.js
+++ pages/api/tag/get.js
@@ -1,6 +1,27 @@
 import { prisma } from "../../../config/prisma";
 
 export default async (req, res) => {
+  const { status } = req.query;
+
+  if (status === "APPROVED") {
+    const response = await prisma.tag.findMany({
+      include: {
+        clubs: {
+          where: {
+            approval: "APPROVED",
+          },
+        },
+      },
+    });
+
+    response.map((tag) => {
+      tag._count = tag.clubs.length;
+      delete tag.clubs;
+    });
+
+    return res.status(200).json([...response]);
+  }
+
   const response = await prisma.tag.findMany({
     include: {
       _count: {
@@ -10,5 +31,6 @@ export default async (req, res) => {
       },
     },
   });
+
   return res.status(200).json([...response]);
 };
