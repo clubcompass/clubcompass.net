9ea208639511600a379d161224d32dcf9e079343
Paul Bokelman <paul.bokelman1@gmail.com>
2022-03-08 22:02:22 -0800

    🧹 removed unused deps in clubApplicationInfoApi

--- server/tests/clubApplicationInfo/clubApplicationInfoApi.ts
+++ server/tests/clubApplicationInfo/clubApplicationInfoApi.ts
@@ -1,14 +1,12 @@
 import * as Client from "@prisma/client";
 import { request } from "graphql-request";
 import { ClubApplicationInfoWhereUniqueInput } from "../../graphql/resolversTypes";
-import { ClubApplicationInfoFields } from "./clubApplicationInfoDocuments";
 import * as doc from "./clubApplicationInfoDocuments";
 const url = "http://localhost:3000/api/graphql";
 
 export const findUniqueClubApplicationInfo = async (
   where: ClubApplicationInfoWhereUniqueInput
 ): Promise<Client.ClubApplicationInfo> => {
-  // console.log(ClubApplicationInfoFields.definitions.map((d) => d.name));
   const { findUniqueClubApplicationInfo: uniqueClubApplicationInfo } =
     await request(url, doc.findUniqueClubApplicationInfo, {
       where,
