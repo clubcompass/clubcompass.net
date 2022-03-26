792eb9cd641dbec5877934cafc1f9ee335f2680d
Paul Bokelman <paul.bokelman1@gmail.com>
2022-03-25 23:52:15 -0700

    🧩 ToastProvider

--- context/ToastProvider.tsx
+++ context/ToastProvider.tsx
@@ -1,34 +1,40 @@
 import {
   useEffect,
   useState,
-  useCallback,
   createContext,
   useContext,
   ReactChild,
 } from "react";
-import { useTransition, animated, config } from "react-spring";
+import { useTransition, animated, config, useSpring } from "react-spring";
 import { IoIosClose } from "react-icons/io";
+import { nanoid } from "nanoid";
 import { MdError } from "react-icons/md";
+import { BsFillCheckCircleFill, BsFillInfoCircleFill } from "react-icons/bs";
+import { TiWarning } from "react-icons/ti";
+import { CgSpinner } from "react-icons/cg";
+
 interface ToastContext {
   addToast(toast: Omit<Toast, "id">): void;
   removeToast(id: Toast["id"]): void;
-  index: number;
 }
 
 export interface Toast {
-  id: number;
-  title: string;
-  message: string;
-  options: {
-    type?: "error" | "success";
-    duration?: number | null; // ms or never go away, default is 3000ms
+  id: string;
+  title?: string;
+  message?: string;
+  // extensions?
+  type?: "error" | "success" | "info" | "warning" | "loading";
+  progress?: boolean;
+  duration?: number | null; // ms or never go away, default is 3000ms
+  custom?: {
+    icon: ReactChild;
+    color: string;
   };
 }
 
 const ToastContext = createContext<ToastContext>({
   addToast: (): void => {},
   removeToast: (): void => {},
-  index: 0,
 });
 
 export const useToastContext = (): ToastContext => {
@@ -37,46 +43,44 @@ export const useToastContext = (): ToastContext => {
 
 export const ToastProvider = ({ children }: { children: ReactChild }) => {
   const [toasts, setToasts] = useState<Toast[]>([]);
-  const [index, setIndex] = useState(0);
 
-  const addToast = useCallback(
-    (toast: Omit<Toast, "id">) => {
-      setToasts((toasts) => [
-        ...toasts,
-        { id: toasts[toasts.length - 1]?.id + 1 || 1, ...toast }, // um...
-      ]);
-      setIndex(index + 1);
-    },
-    [index]
-  );
+  const addToast = (toast: Omit<Toast, "id">) => {
+    setToasts((toasts) => [
+      ...toasts,
+      { id: nanoid(), ...toast }, // um...
+    ]);
+  };
 
-  const removeToast = useCallback((id: Toast["id"]) => {
+  const removeToast = (id: Toast["id"]) => {
     setToasts((toasts) => toasts.filter((toast) => toast.id !== id));
-  }, []);
-
-  useEffect(() => {
-    if (toasts.length === 0) {
-      setIndex(0);
-    }
-  }, [toasts]);
+  };
 
   const value = {
     addToast,
     removeToast,
-    index,
   };
+
+  const transitions = useTransition(toasts, {
+    from: { opacity: 0, transform: "translate(30px, 0px)" },
+    enter: { opacity: 1, transform: "translate(0px, 0px)" },
+    leave: { opacity: 0, transform: "translate(30px, 0px)", display: "none" },
+    config: config.gentle,
+  });
+
+  // animate down smoothly
 
   return (
     <ToastContext.Provider value={value}>
       <Toaster>
-        {toasts.map(({ id, title, message, options }, i) => (
-          <Toast
-            key={i}
-            title={title}
-            message={message}
-            options={options}
-            remove={() => removeToast(id)}
-          />
+        {transitions((style, { id, ...props }) => (
+          <animated.div
+            key={id}
+            style={style}
+            className="toast-message"
+            onClick={() => removeToast(id)}
+          >
+            <Toast key={id} remove={() => removeToast(id)} {...props} />
+          </animated.div>
         ))}
       </Toaster>
       {children}
@@ -84,58 +88,155 @@ export const ToastProvider = ({ children }: { children: ReactChild }) => {
   );
 };
 
+type Toasts = {
+  [key in Toast["type"]]: {
+    icon: ReactChild;
+    color: string;
+    default: {
+      title: Toast["title"];
+      message?: Toast["message"];
+    };
+  };
+};
+
 const Toast = ({
   title,
   message,
-  options,
+  progress,
+  duration,
+  type,
+  custom,
   remove,
-}: {
-  title: Toast["title"];
-  message: Toast["message"];
-  options?: Toast["options"];
-  remove: () => void;
-}) => {
+}: Omit<Toast, "id"> & { remove: () => void }) => {
+  const toasts: Toasts = {
+    error: {
+      icon: <MdError />,
+      color: "#f44336",
+      default: {
+        title: "Something went wrong",
+        message:
+          "An error occurred while processing your request. Please try again later or contact support.",
+      },
+    },
+    success: {
+      icon: <BsFillCheckCircleFill />,
+      color: "#2fba34",
+      default: {
+        title: "Success",
+        message: "Your request was processed successfully.",
+      },
+    },
+    info: {
+      icon: <BsFillInfoCircleFill />,
+      color: "#1C5EF9",
+      default: {
+        title: "Information",
+        message: "This is an information message.",
+      },
+    },
+    warning: {
+      icon: <TiWarning />,
+      color: "#ffbc57",
+      default: {
+        title: "Warning",
+        message: "This action may be unsafe",
+      },
+    },
+    loading: {
+      icon: <CgSpinner className="animate-spin" />,
+      color: "#000000",
+      default: {
+        title: "Loading...",
+      },
+    },
+  };
+
+  const { icon, color } = toasts[type] || {};
+
+  const pbar = useSpring({
+    from: { width: "0%" },
+    to: { width: "100%" },
+    config: { ...config.gentle, duration: duration || 3000 },
+  });
+
   useEffect(() => {
-    if (typeof options?.duration !== "undefined") {
-      if (options.duration === null) {
+    if (typeof duration !== "undefined") {
+      if (duration === null) {
         return;
       }
-      const timeout = setTimeout(remove, options.duration);
+      const timeout = setTimeout(remove, duration);
       return () => clearTimeout(timeout);
     } else {
       const timeout = setTimeout(remove, 3000);
       return () => clearTimeout(timeout);
     }
-  }, [remove, options?.duration]);
+  }, [remove, duration]);
 
   return (
-    <div className="relative w-[25rem] rounded-lg border-l-8 border-red-500 bg-white px-2 py-4 drop-shadow-md">
-      <div className="absolute right-1 top-1">
+    <div
+      style={{
+        // borderLeftWidth: "8px",
+        // borderColor: color,
+        boxShadow: "0px 2px 20px rgb(0 0 0 / 0.10)",
+        maxWidth: "30rem",
+      }}
+      className="relative rounded-lg bg-white px-2 py-4"
+    >
+      <div
+        style={{
+          position: "absolute",
+          right: "4px",
+          top: type === "loading" ? null : "4px",
+        }}
+      >
         <IoIosClose
-          className="z-[60] cursor-pointer text-red-500"
+          className="z-[60] cursor-pointer text-gray-500"
           size={24}
           onClick={remove}
         />
       </div>
-      <div className="flex items-center gap-4">
-        <div>
-          <MdError className="text-red-500" size={25} />
-        </div>
-        <div>
-          <h4 className="text-red-500">{title}</h4>
-          <p>{message}</p>
+      <div
+        style={{
+          marginRight: type === "loading" ? "1.5rem" : "1rem",
+        }}
+        className=" flex items-center gap-2"
+      >
+        <div style={{ color, fontSize: "21px" }}>{icon}</div>
+        <div style={{ width: "90%" }} className="flex flex-col">
+          <h4 style={{ color }} className="font-semibold">
+            {title || toasts[type].default.title}
+          </h4>
+          {type !== "loading" && (
+            <p className="text-sm text-gray-500">
+              {" "}
+              {message || toasts[type].default.message}
+            </p>
+          )}
         </div>
       </div>
+      <span
+        style={{
+          position: "absolute",
+          height: "9px",
+          backgroundColor: color,
+          bottom: 0,
+          left: 0,
+          borderRadius: 3,
+          // width: "50%",
+          // ...pbar,
+        }}
+      />
     </div>
   );
 };
 
-const Toaster = ({ children }: { children: ReactChild[] }) => {
+const Toaster = ({ children }: { children: ReactChild }) => {
   return (
-    <div className="h-full align-baseline">
-      <div className="fixed bottom-4 right-4 z-[60] flex flex-col gap-2">
-        {children}
-      </div>
+    <div
+      style={{ position: "fixed", bottom: "16px", right: "16px", zIndex: 100 }}
+      className="flex flex-col gap-2"
+    >
+      {children}
     </div>
   );
 };
