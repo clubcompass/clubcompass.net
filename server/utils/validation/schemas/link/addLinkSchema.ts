93c6292cb7ad0d47d1513ec90143e8c4ca1cb88a
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2022-04-04 00:00:20 -0700

    🐛 add link bug fixes

--- server/utils/validation/schemas/link/addLinkSchema.ts
+++ server/utils/validation/schemas/link/addLinkSchema.ts
@@ -2,22 +2,26 @@ import { object, string } from "yup";
 
 export const addLinkSchema = object().shape({
   name: string().required("name of link is required"),
-  type: string().oneOf(
-    [
-      "EMAIL",
-      "TWITTER",
-      "INSTAGRAM",
-      "DISCORD",
-      "YOUTUBE",
-      "FACEBOOK",
-      "REMIND",
-      "SNAPCHAT",
-      "WEBSITE",
-    ],
-    "Invalid link type."
-  ),
-  link: string().matches(
-    /^((http|https):\/\/)?(www.)?(?!.*(http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+(\/)?.([\w\?[a-zA-Z-_%\/@?]+)*([^\/\w\?[a-zA-Z0-9_-]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/,
-    "Website should be a valid URL"
-  ),
+  type: string()
+    .oneOf(
+      [
+        "EMAIL",
+        "TWITTER",
+        "INSTAGRAM",
+        "DISCORD",
+        "YOUTUBE",
+        "FACEBOOK",
+        "REMIND",
+        "SNAPCHAT",
+        "WEBSITE",
+      ],
+      "Invalid link type."
+    )
+    .required("link type is required"),
+  link: string()
+    .matches(
+      /^((http|https):\/\/)?(www.)?(?!.*(http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+(\/)?.([\w\?[a-zA-Z-_%\/@?]+)*([^\/\w\?[a-zA-Z0-9_-]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/,
+      "Website should be a valid URL"
+    )
+    .required("link is required"),
 });
