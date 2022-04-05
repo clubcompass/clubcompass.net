3cd4020729b8a6ed231647b2e8da925f3844f8a2
Paul Bokelman <paul.bokelman1@gmail.com>
2022-04-05 01:37:55 -0700

    🧹 Actually fixed clubs

--- components/pages/clubs/ClubsWrapper.jsx
+++ components/pages/clubs/ClubsWrapper.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const ClubsWrapper = ({ children }) => {
+const ClubsWrapper = ({ children }) => {
   return (
     <div className="flex flex-col justify-items-center gap-6 md:grid md:grid-cols-cards">
       {children}
     </div>
   );
 };
+
+export default ClubsWrapper;
--- pages/clubs.tsx
+++ pages/clubs.tsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import type { GetServerSideProps } from "next";
 import type {
   GetApprovedClubsArgs,
   GetApprovedClubsPayload,
 } from "../server/graphql/club/types";
 import { GET_APPROVED_CLUBS } from "../lib/docs/clubDocuments";
-import { useQuery } from "@apollo/client";
-import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useAuthContext, useToastContext } from "../context";
-import { Clubs, ClubsToolbar } from "../components/pages/clubs";
+import ClubsToolbar from "../components/pages/clubs/toolbar/ClubsToolbar";
+import Clubs from "../components/pages/clubs/Clubs";
 import { initializeApollo } from "../lib/apolloClient";
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
@@ -40,37 +39,11 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   };
 };
 
-const Cards = ({ initialClubs }) => {
+const Cards = ({ initialClubs }: { initialClubs: GetApprovedClubsPayload }) => {
   const { user } = useAuthContext();
   const { addToast } = useToastContext();
   const [clubs, setClubs] = useState(initialClubs);
-  const [staticClubs, setStaticClubs] = useState(initialClubs); // static clubs could be abstracted to just be data returned from query
-  // const { loading: clubsLoading } = useQuery(GET_APPROVED_CLUBS, {
-  //   context: {
-  //     ...(user && {
-  //       headers: {
-  //         authorization: `Bearer ${user.token}`,
-  //       },
-  //     }),
-  //   },
-  //   fetchPolicy: "cache-and-network",
-  //   onCompleted: ({ getApprovedClubs: clubs = {} } = {}) => {
-  //     console.log(clubs);
-  //     setClubs(clubs);
-  //     setStaticClubs(clubs);
-  //   },
-  //   onError: (e) => {
-  //     addToast({
-  //       type: "error",
-  //       title: "Error",
-  //       description:
-  //         "There was an error loading the clubs, please try again later.",
-  //     });
-  //     console.log(e);
-  //   },
-  // });
-
-  console.log(initialClubs);
+  const [staticClubs, setStaticClubs] = useState(initialClubs);
 
   return (
     <div className="flex flex-col gap-6">
@@ -83,23 +56,5 @@ const Cards = ({ initialClubs }) => {
     </div>
   );
 };
-
-// const SkeletonTree = () => {
-//   const cards = 12;
-//   return (
-//     <div className="grid grid-cols-cards gap-6">
-//       {Array.from({ length: cards }).map((_, i) => (
-//         <Skeleton
-//           key={i}
-//           height={223}
-//           width="100%"
-//           borderRadius={12}
-//           baseColor="#fafafa"
-//           highlightColor="#eeeeee"
-//         />
-//       ))}
-//     </div>
-//   );
-// };
 
 export default Cards;
